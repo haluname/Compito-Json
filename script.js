cdb0d8b5ec9f79b7078362e75552ff50d800c6a5
Begovic Emanulah <ema13bego@gmail.com>
2023-08-28 16:24:00 +0200

    aggiunta json

---
+++ script.js
@@ -0,0 +1,58 @@
+var film = [
+    {
+        id: 1,
+        titolo: "Il Padrino",
+        regista: "Francis Ford Coppola",
+        anno: 1972,
+        genere: "Drammatico",
+    },
+    {
+        id: 2,
+        titolo: "Forrest Gump",
+        regista: "Robert Zemeckis",
+        anno: 1994,
+        genere: "Commedia",
+    },
+    {
+        id: 3,
+        titolo: "Pulp Fiction",
+        regista: "Quentin Tarantino",
+        anno: 1994,
+        genere: "Crimine",
+    },
+    {
+        id: 4,
+        titolo: "La Vita è Bella",
+        regista: "Roberto Benigni",
+        anno: 1997,
+        genere: "Commedia",
+    },
+    {
+        id: 5,
+        titolo: "Inception",
+        regista: "Christopher Nolan",
+        anno: 2010,
+        genere: "Fantascienza",
+    },
+    {
+        id: 6,
+        titolo: "Il Signore degli Anelli: La Compagnia dell'Anello",
+        regista: "Peter Jackson",
+        anno: 2001,
+        genere: "Fantasy",
+    },
+    {
+        id: 7,
+        titolo: "Star Wars: Episodio IV - Una Nuova Speranza",
+        regista: "George Lucas",
+        anno: 1977,
+        genere: "Fantascienza",
+    },
+    {
+        id: 8,
+        titolo: "La La Land",
+        regista: "Damien Chazelle",
+        anno: 2016,
+        genere: "Musical",
+    }
+];
