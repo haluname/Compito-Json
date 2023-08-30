cc20883afa3cd14347e7793b87f6ef2948f695bb
Begovic Emanulah <ema13bego@gmail.com>
2023-08-30 09:20:38 +0200

    aggiunta ricerca e ordinamento

--- script.js
+++ script.js
@@ -61,33 +61,40 @@ let oggetto = {}
 let txtAggiungi = document.querySelector("#aggiungiFilm")
 let txtRimuovi = document.querySelector("#rimuoviFilm")
 let txtModifica = document.querySelector("#modificaFilm")
+let txtCerca = document.querySelector("#cercaFilm")
 let listaFilm = document.querySelector(".princ")
+let selectModo = document.querySelector("#modoOrdinamento")
+let selectCategoria = document.querySelector("#tipoOrdinamento")
+let intervallo;
 
-setInterval(function(){
-    listaFilm.innerHTML= "";
-    listaFilm.innerHTML+="<h2>Lista Film:</h2>"
-    for(let i=0; i<film.length; i++){
-        listaFilm.innerHTML+=`<span>${film[i].id}</span>`
-        listaFilm.innerHTML+=`<span>${film[i].titolo}</span>`
-        listaFilm.innerHTML+=`<span>${film[i].regista}</span>`
-        listaFilm.innerHTML+=`<span>${film[i].anno}</span>`
-        listaFilm.innerHTML+=`<span>${film[i].genere}</span>`
-        listaFilm.innerHTML+=`<br>`
+intervallo = setInterval(aggiornaListaFilm, 1000);
+
+function aggiornaListaFilm() {
+    listaFilm.innerHTML = "";
+    listaFilm.innerHTML += "<h2>Lista Film:</h2>"
+    for (let i = 0; i < film.length; i++) {
+        listaFilm.innerHTML += `<span>${film[i].id}</span>`
+        listaFilm.innerHTML += `<span>${film[i].titolo}</span>`
+        listaFilm.innerHTML += `<span>${film[i].regista}</span>`
+        listaFilm.innerHTML += `<span>${film[i].anno}</span>`
+        listaFilm.innerHTML += `<span>${film[i].genere}</span>`
+        listaFilm.innerHTML += `<br>`
     }
-},1000)
+}
 
 
-function mandaDati(e){
+function mandaDati(e) {
     let error = false;
 
-    if(e.key=="Enter"){
+    if (e.key == "Enter") {
         let array = txtAggiungi.value.split(',');
-        if(array.length == 5){
-            for(let i=0; i<film.length; i++){
-                if(array[0] == film[i].id.toString())
-                error = true;
+        if (array.length == 5) {
+            for (let i = 0; i < film.length; i++) {
+                if (array[0] == film[i].id.toString())
+                    error = true;
             }
-            if(!error){
+            if (!error) {
+                oggetto = {}
                 oggetto.id = parseInt(array[0]);
                 oggetto.titolo = array[1].trim();
                 oggetto.regista = array[2].trim();
@@ -95,44 +102,44 @@ function mandaDati(e){
                 oggetto.genere = array[4].trim();
                 film.push(oggetto);
                 console.log(film)
-                localStorage.setItem('film', JSON.stringify(film));
+                //localStorage.setItem('film', JSON.stringify(film));
 
             }
-            else{
+            else {
                 alert("hai inserito un film con id già esistente")
             }
-           
+
         }
-        else{
+        else {
             console.log("nada")
         }
-       
+
     }
 }
 
 
-function eliminaDati(e){
-    if(e.key=="Enter"){
-        for(let i=0; i<film.length; i++){
-            if(film[i].id.toString() == txtRimuovi.value){
+function eliminaDati(e) {
+    if (e.key == "Enter") {
+        for (let i = 0; i < film.length; i++) {
+            if (film[i].id.toString() == txtRimuovi.value) {
                 if (window.confirm("Vuoi eseguire questa azione?")) {
-                    film.splice(i,1) 
-                    localStorage.setItem('film', JSON.stringify(film));
+                    film.splice(i, 1)
+                    //localStorage.setItem('film', JSON.stringify(film));
 
-                  } else {
+                } else {
                     alert("azione annullata")
-                  }
-                  
+                }
+
             }
         }
     }
 }
 
 
-function modificaDati(e){
-    if(e.key=="Enter"){
-        for(let i=0; i<film.length; i++){
-            if(film[i].id.toString() == txtModifica.value){
+function modificaDati(e) {
+    if (e.key == "Enter") {
+        for (let i = 0; i < film.length; i++) {
+            if (film[i].id.toString() == txtModifica.value) {
                 film[i].titolo = prompt("Inserisci il nuovo titolo:");
                 film[i].regista = prompt("Inserisci il nuovo regista:");
                 film[i].anno = prompt("Inserisci il nuovo anno:");
@@ -140,15 +147,64 @@ function modificaDati(e){
             }
         }
     }
- 
+
 
 }
 
 
-
+/*
 window.onload = function () { //all onload prendo in filmArchiviati lo storage di "film", se non c'è allora lo ignora, ma se c'è qualcosa nello storage allora riprende la variabile film e ci infila il JSON
     var filmArchiviati = localStorage.getItem('film');
     if (filmArchiviati) {
         film = JSON.parse(filmArchiviati);
     }
-};  
+};  */
+
+function ordinaArray(e) {
+    let attributo = selectCategoria.value;
+
+    film.sort(function (a, b) {
+        if (selectModo.value === "Crescente") {
+            if (a[attributo] < b[attributo]) return -1;
+            if (a[attributo] > b[attributo]) return 1;
+            else return 0;
+        } else {
+            if (b[attributo] < a[attributo]) return -1;
+            if (b[attributo] > a[attributo]) return 1;
+            else return 0;
+        }
+    });
+}
+
+function ricercaDati(e) {
+    clearInterval(intervallo);
+    listaFilm.innerHTML = "";
+    const valoreRicerca = txtCerca.value.toLowerCase();
+
+    for (let i = 0; i < film.length; i++) {
+        for (let proprieta in film[i]) {
+            let valoreProprieta = film[i][proprieta].toString().toLowerCase();
+
+            if (valoreProprieta.includes(valoreRicerca)) {
+                listaFilm.innerHTML += `<span>${film[i].id}</span>`;
+                listaFilm.innerHTML += `<span>${film[i].titolo}</span>`;
+                listaFilm.innerHTML += `<span>${film[i].regista}</span>`;
+                listaFilm.innerHTML += `<span>${film[i].anno}</span>`;
+                listaFilm.innerHTML += `<span>${film[i].genere}</span>`;
+                listaFilm.innerHTML += `<br>`;
+                break;
+            }
+        }
+    }
+}
+
+txtCerca.addEventListener("focus", function() {
+    clearInterval(intervalloListaFilm);
+});
+
+txtCerca.addEventListener("blur",function(){
+    txtCerca.value = "";
+    intervalloListaFilm = setInterval(aggiornaListaFilm, 1000);
+})
+
+
